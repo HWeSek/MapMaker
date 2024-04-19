40f31a6e92b3eaaa8bd739fd99b2c13ff595571f
Adam <cichon.adam0205@gmail.com>
2024-04-19 10:50:30 +0200

    more

--- src/item.ts
+++ src/item.ts
@@ -1,11 +1,23 @@
-import SpriteSheet from '/gfx/sprites.png';
+import SpriteSheet from '/sprites.png';
 
-export default class Item extends HTMLDivElement {
-    public position : object;
-    public type : object;
-    constructor(position: object, id: object){
-        super()
+export default class Item {
+    public position : object
+    public type : number
+    constructor(position: object, id: number){
         this.position = position;
         this.type = id;
+    }
+
+    private positionFromId(id:number) : string {
+        const width = 
+        return 'string';
+    }
+    public createElement() {
+        let item : HTMLDivElement = document.createElement('div');
+        item.classList.add('item');
+        item.style.backgroundImage = `url(${SpriteSheet})`;
+        item.style.backgroundPositionX = `-${(this.position.x-1)*24}px`
+        item.style.backgroundPositionY = `-${(this.position.y-1)*24}px`
+        document.getElementById('items')?.append(item);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Item from './item';
 
 window.addEventListener('DOMContentLoaded', ()=>{
+    let counter : number = 0;
     for(let i : number = 1; i <= 40; i++){
         for(let j : number = 1; j <= 16; j++){
-            let item : HTMLDivElement = document.createElement('div');
-            item.classList.add('item');
-            document.getElementById('items')?.append(item);
+            let item = new Item({x: j,y: i}, counter);   
+            item.createElement();
+            counter ++;
         }
     }
 });
