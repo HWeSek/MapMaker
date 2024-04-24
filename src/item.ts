589af2c7afda65e0f7513dea4b28092ccac7cac1
HWeSek <cichon.adam0205@gmail.com>
2024-04-24 14:17:48 +0200

    items are now working

--- src/item.ts
+++ src/item.ts
@@ -1,23 +1,43 @@
 import SpriteSheet from '/sprites.png';
+import { Data } from './main';
+
+interface Coordinates {
+    x: number,
+    y: number
+}
 
 export default class Item {
-    public position : object
-    public type : number
-    constructor(position: object, id: number){
+    public position: object
+    public type: number
+    constructor(position: Coordinates, id: number) {
         this.position = position;
         this.type = id;
     }
 
-    private positionFromId(id:number) : string {
-        const width = 
-        return 'string';
+    private positionFromId(id: number): Coordinates {
+        let row: number;
+        let col: number;
+        if (id <= 320) {
+            row = Math.floor(id / 16)
+            col = id - (16 * row)
+        } else {
+            row = Math.floor((id - 320) / 16)
+            col = id - 320 - (16 * row) + 16
+        }
+        return { x: col, y: row };
     }
+
     public createElement() {
-        let item : HTMLDivElement = document.createElement('div');
+        let item: HTMLDivElement = document.createElement('div');
         item.classList.add('item');
         item.style.backgroundImage = `url(${SpriteSheet})`;
-        item.style.backgroundPositionX = `-${(this.position.x-1)*24}px`
-        item.style.backgroundPositionY = `-${(this.position.y-1)*24}px`
+        item.style.backgroundPositionX = `-${this.positionFromId(this.type).x * 24}px`
+        item.style.backgroundPositionY = `-${this.positionFromId(this.type).y * 24}px`
+        item.addEventListener('click', () => {
+            document.querySelectorAll('.item').forEach((element) => { element.classList.remove('selected') })
+            item.classList.add('selected');
+            Data.selected_item_type = this.type;
+        })
         document.getElementById('items')?.append(item);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import Item from './item';
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    let counter : number = 0;
-    for(let i : number = 1; i <= 40; i++){
-        for(let j : number = 1; j <= 16; j++){
-            let item = new Item({x: j,y: i}, counter);   
+interface Data {
+    selected_item_type: number
+    selected_items: Array<object>
+}
+
+
+const Data: Data = {
+    selected_item_type: -1,
+    selected_items: [],
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    let counter: number = 0;
+    for (let i: number = 1; i <= 40; i++) {
+        for (let j: number = 1; j <= 16; j++) {
+            let item = new Item({ x: j, y: i }, counter);
             item.createElement();
-            counter ++;
+            counter++;
         }
     }
-});
+}, { once: true });
+
+export { Data };
